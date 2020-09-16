01080627a0bd75d70b8cc2c701f9bab7972704fc
sjk <sjk@sjkui-MacBookAir.local>
2020-09-16 16:44:16 +0900

    add reviews app

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ PROJECT_APPS = [
     "core.apps.CoreConfig",
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
+    "reviews.apps.ReviewsConfig",
 ]
 THIRD_PARTY_APPS = ["django_countries"]
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
--- rooms/admin.py
+++ rooms/admin.py
@@ -2,8 +2,27 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
+    pass
+
+
 @admin.register(models.Room)
-class roomAdmin(admin.ModelAdmin):
+class RoomAdmin(admin.ModelAdmin):
+
+    """ Room Admin Definition """
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """ """
+
     pass
 
 
