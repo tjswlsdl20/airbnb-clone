ac9cbc1936b23d1f40217ab6ef0c089c7efad190
sjk <tjswlsdl20@gmail.com>
2020-10-07 21:28:02 +0900

    fix searchview

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
-from django.views.generic import ListView
-from django.urls import reverse
-from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django_countries import countries
 from . import models
 
 
@@ -15,15 +15,19 @@ class HomeView(ListView):
     context_object_name = "rooms"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        return redirect(reverse("core:home"))
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
 
 
 def search(request):
-    city = request.GET.get("city")
+    city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
-    return render(request, "rooms/search.html", {"city": city})
+    room_types = models.RoomType.objects.all()
+    return render(
+        request,
+        "rooms/search.html",
+        {"city": city, "countries": countries, "room_types": room_types},
+    )
